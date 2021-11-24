047b26ad0117ca4c8ea408b8d7b05d0e60b5c0d2
Ste2812 <stefanogallone@hotmail.com>
2021-11-24 11:00:43 +0100

    ejercicio 2niv1 php

--- php2.php
+++ php2.php
@@ -1,17 +1,34 @@
 <?php
 
+function evaluar(){
+
 $evaluarValor=31;
 
-if($evaluarValor%2==0){
+    if($evaluarValor%2==0){
     echo "El numero es par.";
 
-}else if(!is_numeric($evaluarValor)){
+    }else if(!is_numeric($evaluarValor)){
     echo "No es un valor numerico.";
 
-}else{
+    }else{
     echo "El numero es impar."; 
+    }
 }
+evaluar();
 
 echo "<br>";
 echo "<hr>";
+
+//ejercicio 2
+
+function contador($valorFinal){
+
+    for($i=0; $i<=$valorFinal; $i+=2){
+        echo "$i";
+        echo ", ";
+    }
+
+}
+contador(10);
+
 ?>
